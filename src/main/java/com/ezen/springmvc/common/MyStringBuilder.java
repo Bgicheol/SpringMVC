d7f52b25f335ee91ea2dcf3a92e22d9c20da9250
Bgicheol <132882312+Bgicheol@users.noreply.github.com>
2023-07-03 17:18:14 +0900

    Initial commit

---
+++ src/main/java/com/ezen/springmvc/common/MyStringBuilder.java
@@ -0,0 +1,11 @@
+package com.ezen.springmvc.common;
+
+public class MyStringBuilder {
+	
+	public static String reverse(String str) {
+		
+	    return new StringBuilder(str).reverse().toString();
+				
+	}
+	
+}
